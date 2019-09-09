d96aa61de176ab406ed8fcaebbecfe7b24bd5aeb
awesomedana <45408015+awesomedana@users.noreply.github.com>
2019-09-09 17:14:28 +0900

    Add files via upload

---
+++ treeSetTest/TreeSet3.java
@@ -0,0 +1,28 @@
+package treeSetTest;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSet3 {
+
+	public static void main(String[] args) {
+		TreeSet<String> treeSet = new TreeSet<String>();
+		treeSet.add("apple");
+		treeSet.add("forever");
+		treeSet.add("description");
+		treeSet.add("ever");
+		treeSet.add("demon");
+		treeSet.add("catch");
+		treeSet.add("zoo");
+		treeSet.add("base");
+		treeSet.add("guess");
+		treeSet.add("cherry");
+
+		System.out.println("[c~f 사이의 단어 검색]");
+		NavigableSet<String> rangeSet = treeSet.subSet("c", true, "f", true);
+		for(String word : rangeSet) {
+			System.out.println(word);
+		}
+	}
+
+}
