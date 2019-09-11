587f25daf29b38c87ee209ccc80d9bd2eceb926e
awesomedana <45408015+awesomedana@users.noreply.github.com>
2019-09-11 14:09:57 +0900

    Add files via upload

---
+++ fruitTreeSetTest/Fruit.java
@@ -0,0 +1,12 @@
+package fruitTreeSetTest;
+
+public class Fruit {
+	public String name;
+	public int price;
+	
+	public Fruit(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+}
